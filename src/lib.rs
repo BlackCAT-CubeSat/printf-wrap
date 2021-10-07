b2cc8be0b1849cc19b2184674f6e24093ca401ff
Zachary Catlin <zec0@psu.edu>
2021-10-07 15:10:06 -0400

    Added examples of using this crate for safe wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -883,3 +883,51 @@ const fn is_null_terminated(s: &[c_char]) -> bool {
     // If we get here, there's no null character at all:
     false
 }
+
+/// An example of how to use the types and traits of this crate to safely
+/// wrap functions with printf(3)-style format strings and varargs.
+pub mod example {
+    use libc::{c_char, c_int};
+    use crate::{PrintfFmt, PrintfArgs, PrintfArgument};
+
+    macro_rules! tuple_impl {
+        ($num:tt, $p_name:ident, $snp_name:ident, ( $($t:ident),* ), ( $($var:ident),* )) => {
+            #[doc = concat!("A safe wrapper around [`printf`](libc::printf) for ", stringify!($num), " argument(s).")]
+            #[inline]
+            pub fn $p_name<$($t),*>(fmt: PrintfFmt<($($t,)*)>, $($var: $t),*) -> c_int
+                where $($t: PrintfArgument ,)*
+                    ($($t,)*): PrintfArgs {
+
+                unsafe {
+                    libc::printf(fmt.as_ptr() $(, $var.as_c_val())* )
+                }
+            }
+
+            #[doc = concat!("A safe wrapper around [`snprintf`](libc::snprintf) for ", stringify!($num), " argument(s).")]
+            #[inline]
+            pub fn $snp_name<$($t),*>(dst: &mut [u8], fmt: PrintfFmt<($($t,)*)>, $($var: $t),*) -> c_int
+                where $($t: PrintfArgument ,)*
+                    ($($t,)*): PrintfArgs {
+
+                unsafe {
+                    libc::snprintf(
+                        dst.as_mut_ptr() as *mut c_char,
+                        dst.len(),
+                        fmt.as_ptr()
+                        $(, $var.as_c_val())*
+                    )
+                }
+            }
+        };
+    }
+
+    tuple_impl!(0, printf0, snprintf0, (), ());
+    tuple_impl!(1, printf1, snprintf1, (A), (a));
+    tuple_impl!(2, printf2, snprintf2, (A, B), (a, b));
+    tuple_impl!(3, printf3, snprintf3, (A, B, C), (a, b, c));
+    tuple_impl!(4, printf4, snprintf4, (A, B, C, D), (a, b, c, d));
+    tuple_impl!(5, printf5, snprintf5, (A, B, C, D, E), (a, b, c, d, e));
+    tuple_impl!(6, printf6, snprintf6, (A, B, C, D, E, F), (a, b, c, d, e, f));
+    tuple_impl!(7, printf7, snprintf7, (A, B, C, D, E, F, G), (a, b, c, d, e, f, g));
+    tuple_impl!(8, printf8, snprintf8, (A, B, C, D, E, F, G, H), (a, b, c, d, e, f, g, h));
+}
